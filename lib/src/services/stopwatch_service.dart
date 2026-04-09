f116f9fec6326d4f2413e4d73e6bcb51ee608dea
Pedro Morais <pedro.j.morais@present-technologies.com>
2026-04-09 22:10:10 +0100

    chore: add todo for services refactor

---
+++ lib/src/services/stopwatch_service.dart
@@ -0,0 +1,2 @@
+// TODO (Pedro Morais - 2026-04-09): factory, one service per stopwatch?
+class StopwatchService {}
---
+++ lib/src/services/task_scheduler_service.dart
@@ -0,0 +1,2 @@
+// TODO (Pedro Morais - 2026-04-09): Manages many tasks
+class TaskSchedulerService {}
